6ac84215bc94ff4761078a9f476e4ee6856a8818
hossen29 <hossenjjjhossen@gmail.com>
2021-01-31 14:47:27 +0600

    new commit korlam

---
+++ hossen.js
@@ -0,0 +1,39 @@
+const out = document.getElementById("sub");
+out.addEventListener("click",function(){
+    const practice = document.getElementById("input-fild");
+    practice.style.display = "none";
+        const tran = document.getElementById("transec");
+        tran.style.display = "block";
+});
+//Deposit button handlar
+const depositBtn = document.getElementById("dipusit-btn");
+depositBtn.addEventListener("click",function(){
+  
+  const depositAmount = updateValue ("diposit");
+  getSpanText ("balance",depositAmount);
+  getSpanText ("current-deposit",depositAmount);
+  document.getElementById("diposit").value = "";
+})
+//widhrow button handlar item
+const widrowBtn = document.getElementById("widhrow-btn");
+widrowBtn.addEventListener("click",function(){
+   
+  const widhrowNamber = updateValue ("widhrow");
+  getSpanText ("current-widrom",widhrowNamber);
+  getSpanText ("balance",-1*widhrowNamber);
+  document.getElementById("widhrow").value = "";
+})
+
+
+ function updateValue (id){
+  const widhrowAmount = document.getElementById(id).value;
+  const widhrowNewNamber = parseFloat(widhrowAmount);
+   return widhrowNewNamber;
+ };
+
+function getSpanText (id,depositAmount){
+  const currentDeposit = document.getElementById(id).innerText;
+  const currentAmount = parseFloat(currentDeposit);
+  const total = depositAmount + currentAmount;
+  document.getElementById(id).innerText = total;
+}
